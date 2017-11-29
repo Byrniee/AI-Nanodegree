2a2a6ba3f56b05fb16b43742cc9fd6f4f47fad44
James Byrne <james.byrne@holovis.com>
2017-11-29 20:08:58 +0000

    Simple puzzle complete

--- Sudoku/utils.py
+++ Sudoku/utils.py
@@ -1,6 +1,8 @@
 rows = 'ABCDEFGHI'
 cols = '123456789'
 
+def cross(a, b):
+    return [s+t for s in a for t in b]
 
 boxes = cross(rows, cols)
 
@@ -12,8 +14,28 @@ units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
 
 
-def cross(a, b):
-    return [s+t for s in a for t in b]
+def reduce_puzzle(values):
+    stalled = False
+    
+    while not stalled:
+        # Check how many boxes have a determined value
+        solvedValuesBefore = len([box for box in values.keys() if len(values[box]) == 1])
+
+        # Your code here: Use the Eliminate Strategy
+        values = eliminate(values)
+
+        # Your code here: Use the Only Choice Strategy
+        values = onlyChoice(values)
+
+        # Check how many boxes have a determined value, to compare
+        solvedValuesAfter = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solvedValuesBefore == solvedValuesAfter
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+
+    return values
 
 
 def onlyChoice(values):
